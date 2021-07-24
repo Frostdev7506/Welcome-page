b7711c87ded6580d4eb64ac9403b32e883c28ad5
Frostdev7506 <neerajbutola234@gmail.com>
2021-07-24 15:57:03 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function()
+	{
+
+		$('#slides').superslides({
+			animation: 'fade',
+			play: 5000 
+
+		});
+
+var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+
+
+	});
